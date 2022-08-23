6dcdb841cec89eaaadb5dcb99b1e08049111de93
andres espinel <moneda19463@gmail.com>
2022-08-23 12:05:09 -0500

    Se hizo el conversor de grados

---
+++ js/grados.js
@@ -0,0 +1,51 @@
+function calcular(){
+    let num1=parseInt(document.getElementById("num1").value);
+    let op=document.getElementById("operacion").value;
+    let op1=document.getElementById("operacion1").value;
+
+    if(op==1 && op1==1){
+        alert(`No se puede convertir ya que son los mismos`);
+    }
+
+    else if(op==1 && op1==2){
+
+        let res= ((5*(num1-32))/9);
+        alert(`${res}`);
+    }
+
+    else if(op==1 && op1==3){
+        let res=((5*(num1-32)/9)+273.15);
+        alert(`${res}`);
+    }
+
+
+
+    else if(op==2 && op1==1){
+        let res=((9*num1)/5)+32;
+        alert(`${res}`);
+    }
+
+    else if(op==2 && op1==2){
+        alert(`No se puede convertir ya que son los mismos`);
+    }
+
+    else if(op==2 && op1==3){
+        let res=num1+273.15;
+        alert(`${res}`);
+    }
+
+
+    else if(op==3 && op1==1){
+        let res= ((9*(num1-273.15))/5)+32;
+        alert(`${res}`);
+    }
+
+    else if(op==3 && op1==2){
+        let res=num1-273.15;
+        alert(`${res}`);
+    }
+
+    else if(op==3 && op1==3){
+        alert(`No se puede convertir ya que son los mismos`);
+    }
+}
